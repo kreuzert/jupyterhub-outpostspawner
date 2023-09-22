6f663dfb07d8e3858a1ae310d82aa36370109121
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2023-09-22 15:11:45 +0200

    0.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import find_packages
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-share_jupyterhub = os.path.join(here, 'share', 'jupyterhub')
+share_jupyterhub = os.path.join(here, "share", "jupyterhub")
+
 
 def get_data_files():
     """Get data files in share/jupyterhub"""
@@ -15,9 +16,10 @@ def get_data_files():
         data_files.append((rel_d, [os.path.join(rel_d, f) for f in filenames]))
     return data_files
 
+
 setup(
     name="jupyterhub-outpostspawner",
-    version="0.0.1.dev1",
+    version="0.0.1",
     description="JupyterHub Spawner to run services on multiple remote resources.",
     url="https://github.com/kreuzert/jupyterhub-outpostspawner",
     author="Tim Kreuzer",
@@ -25,7 +27,7 @@ setup(
     license="3-BSD",
     data_files=get_data_files(),
     packages=find_packages(),
-    install_requires=["jupyterhub","traitlets","escapism","kubernetes"],
+    install_requires=["jupyterhub", "traitlets", "escapism", "kubernetes"],
     python_requires=">=3.9",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
